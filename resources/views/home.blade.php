1a4f51be4e6ca8563a0d137421a3bd32223fbd4c
Kumar Dzhunushaliev <d.kumarkhan@gmail.com>
2020-09-08 13:34:03 +0600

    +

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -244,7 +244,7 @@
 					<div class="ui middle aligned stackable grid container">
 						<div class="row">
 							<div class="eight wide column">
-								<h3 class="ui header">Польза цифровизации</h3>
+								<h3 class="ui header">Важность цифровизации</h3>
 								<p>We can give your company superpowers to do things that they never thought possible. Let us
 									delight your customers and empower your needs...through pure data analytics.</p>
 								<h3 class="ui header">Роль информационных технологий в сельском хозяйстве</h3>
